b83395779517790562ef75dc66c7f8b58885fe56
Agostinho Dalte Baptista Soberano <agostinhosoberano2@gmail.com>
2023-07-08 13:05:01 +0200

    adding javascript interactivity

---
+++ script.js
@@ -0,0 +1,72 @@
+import { menuArray } from "./data.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+const orders = [];
+
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.plus) {
+    handlePlusButtonClick(e.target.dataset.plus);
+  }
+  if (e.target.dataset.remove) {
+    handleRemoveClick(e.target.dataset.remove);
+  }
+});
+
+function handlePlusButtonClick(id) {
+  const exactItem = menuArray.find((item) => item.id == id);
+  orders.push({
+    id: uuidv4(),
+    name: exactItem.name,
+    price: exactItem.price,
+  });
+  render();
+}
+
+function handleRemoveClick(id) {
+  const exactOrder = orders.find((order) => order.id == id);
+  const index = orders.indexOf(exactOrder);
+  orders.splice(index, 1);
+  render();
+}
+
+function getMenuItems() {
+  let items = "";
+  menuArray.forEach((item) => {
+    items += `
+    <div class="items-list">
+          <div class="item-emoji">${item.emoji}</div>
+          <div class="item-description">
+            <h2>${item.name}</h2>
+            <p class="item-ingredients">${item.ingredients.join(", ")}
+            </p>
+            <p>$${item.price}</p>
+          </div>
+          <div class="rounded-plus"><i class="fa-regular fa-plus" data-plus="${
+            item.id
+          }"></i></div>
+    </div>  
+    `;
+  });
+  return items;
+}
+
+function getOrders() {
+  let orderHtml = "";
+  orders.forEach((order) => {
+    orderHtml += `
+        <div class="order-item">
+        <p class="order-name">${order.name}</p>
+        <p class="order-remove" data-remove="${order.id}">remove</p>
+        <p class="order-price">$${order.price}</p>
+      </div>
+        `;
+  });
+  return orderHtml;
+}
+
+function render() {
+  document.getElementById("itemsEl").innerHTML = getMenuItems();
+  document.getElementById("ordersEl").innerHTML = getOrders();
+}
+
+render();
